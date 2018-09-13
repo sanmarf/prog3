7ea1b44e2d1c2863f834d9de0a15d80fb1d74d44
Pedro J. Ponce de León <pierre@dlsi.ua.es>
2018-09-13 14:25:24 +0200

    Main class for Tetris assignment

--- src/mains/Main1.java
+++ src/mains/Main1.java
@@ -1,8 +1,11 @@
+/**
+ * Main package for the first assignment.
+ */
 package mains;
 
 import java.util.ArrayList;
 
-import modelo.Coordenada;
+import model.Coordinate;
 
 /**
 @author drizo
@@ -10,17 +13,17 @@ import modelo.Coordenada;
 public class Main1 {
 
 	/**
-	 * Clase principal de la P1
-	 * @param args
+	 * Entry method.
+	 * @param args Arguments from command line.
 	 */
 	public static void main(String[] args) {
-		//Coordenada c0; // no se instancia porque no se ha hecho new
-		Coordenada c1 = new Coordenada(0,0);
-		Coordenada c2 = new Coordenada(10,10);
-		Coordenada c3 = new Coordenada(4,3);
-		Coordenada c4 = new Coordenada(5,15);
-		Coordenada c5 = new Coordenada(c4);
-		Coordenada c6 = new Coordenada(2,5);
+		Coordinate c0; // no instance created for c0 
+		Coordinate c1 = new Coordinate(0,0);
+		Coordinate c2 = new Coordinate(10,10);
+		Coordinate c3 = new Coordinate(4,3);
+		Coordinate c4 = new Coordinate(5,15);
+		Coordinate c5 = new Coordinate(c4);
+		Coordinate c6 = new Coordinate(2,5);
 		System.out.println(c1.toString());
 		System.out.println();
 		
@@ -36,30 +39,32 @@ public class Main1 {
 		System.out.println(c5.toString());
 		System.out.println();
 		
-		Coordenada sumada = c3.suma(c6);
-		System.out.println("Suma de " + c3.toString() + "+" + c6.toString() + "= " + sumada.toString());
+		Coordinate s = c3.add(c6);
+		System.out.println(c3.toString() + "+" + c6.toString() + "= " + s.toString());
 		System.out.println();
 		
-		Coordenada [] v = new Coordenada[5];
+		Coordinate [] v = new Coordinate[5];
 		for (int i=0; i<5; i++) {
-			v[i] = new Coordenada(i,4-i);
+			v[i] = new Coordinate(i,4-i);
 		}
 		
 		for (int i=0; i<5; i++) {
-			System.out.println(v[i].getX() + ", " + v[i].getY());
+			System.out.println(v[i].getRow() + "," + v[i].getColumn());
 		}
 
-		ArrayList<Coordenada> v2 = new ArrayList<Coordenada>();
+		ArrayList<Coordinate> v2 = new ArrayList<Coordinate>();
 		for (int i=0; i<8; i++) {
-			v2.add(new Coordenada(i, i));
+			v2.add(new Coordinate(i, i));
 			System.out.println(v2.get(i).toString());
 		}
 		
-		Coordenada c7 = c1.suma(c3);
+		Coordinate c7 = c1.add(c3);
 		if (c7.equals(c3)) {
-			System.out.println("c7 y c3 son iguales");
+			System.out.println("c7 and c3 are equal");
 		}
 
-		System.out.println("Num.total de coordenadas: " + Coordenada.getNumeroCoordenadas());		
+		System.out.println("Coordinate count: " + Coordinate.getCoordinateCount());
+		
 	}
+
 }
